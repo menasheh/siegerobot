a1a8c3f9c4e6070d459f0d413ff69dee6b1514f7
Menasheh Peromsik <menashehyperomsik@gmail.com>
2018-07-22 18:28:51 -0400

    add plaintext http status on 8080

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from aiohttp import web
 from os import environ
 from os.path import expanduser
 import sys
@@ -29,9 +30,7 @@ def get_config():
 
 
 configs = get_config()
-
 clients = []
-
 for config in configs:
     print("starting session '" + config[0] + "'")
     clients.append([
@@ -44,11 +43,32 @@ for config in configs:
         modes[int(config[1])]
     ])
 
-robots = [Siege(client[0], client[1]).run() for client in clients]
+sieges = [Siege(client[0], client[1]) for client in clients]
+routines = [siege.run() for siege in sieges]
+
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "No session for " + name
+    for siege in sieges:
+        if siege.telegram.session.filename.split('.')[0] == name:
+            text = ""
+            for k, v in siege.city.__dict__.items():
+                text += str(k) + ": " + str(v) + "\n"
+            continue
+    return web.Response(text=text)
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+runner = web.AppRunner(app)
 
 
 async def main():
-    await asyncio.gather(*robots)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 8080)
+    await site.start()
+    await asyncio.gather(*routines)
+    await runner.cleanup()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
